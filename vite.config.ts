3cbc4e5b68221d2c01d9ae4f62535dab227e9967
Juan Deossa <jedp082@gmail.com>
2025-06-12 20:58:52 -0500

    [DEPLOY]: added gh pages to quick deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwiindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwiindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/global66-technical-test/", // Comment this line if you want to use the default base path or dev server
   plugins: [vue(), tailwiindcss()],
-})
+});
